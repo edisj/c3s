3c7cccbf9473344bc8a064cba9d5902c56766310
edisj <ejakupov@asu.edu>
2022-09-18 23:26:32 -0700

    modify tests

--- testing/test_master_equation.py
+++ testing/test_master_equation.py
@@ -3,8 +3,9 @@ Unit and regression test for the c3s package.
 """
 import pytest
 import sys
-import c3s
 import numpy as np
+import math
+from c3s.simulators import CME
 from numpy.testing import assert_almost_equal, assert_equal, assert_array_almost_equal
 
 
@@ -17,7 +18,7 @@ class TestMutualInformationWithSyntheticData:
 
     @pytest.fixture(scope='class')
     def synthetic_system(self):
-        empty_system = c3s.simulators.MasterEquation(empty=True)
+        empty_system = CME(empty=True)
 
         # make up fake species
         species = ['X', 'Y', 'Z']
@@ -42,7 +43,7 @@ class TestMutualInformationWithSyntheticData:
         MI_0 = 0              # 1st timestep
         MI_1 = 0.691244272798 # 2nd timestep
 
-        MIs = synthetic_system.calculate_mutual_information(['X'], ['Y'])
+        MIs = synthetic_system.calculate_mutual_information(['X'], ['Y'], base=math.e)
 
         assert_almost_equal(MI_0, MIs[0], decimal=5)
         assert_almost_equal(MI_1, MIs[1], decimal=5)
@@ -52,14 +53,14 @@ class TestBinarySystem:
 
     @pytest.fixture(scope='class')
     def binary(self):
-        system =  c3s.simulators.MasterEquation(cfg='config_files/binary.yml', A=1)
+        system =  CME(cfg='config_files/binary.yml', initial_populations={'A':1})
         start, stop, step = 0, 1, 0.01
         system.run(start, stop, step)
         yield system
 
     @pytest.fixture(scope='class')
     def binary_many_body(self):
-        system = c3s.simulators.MasterEquation(cfg='config_files/binary.yml', A=3)
+        system = CME(cfg='config_files/binary.yml', initial_populations={'A':3})
         start, stop, step = 0, 1, 0.01
         system.run(start, stop, step)
         yield system
@@ -90,13 +91,13 @@ class TestBinarySystem:
     def test_changing_rates(self, binary):
 
         rate1, rate2 = 2, 2
-        binary.update_rates(k_1=rate1, k_2=rate2)
+        binary.update_rates(dict(k_1=rate1, k_2=rate2))
         assert_equal(binary.rates[0][1], rate1)
         assert_equal(binary.rates[1][1], rate2)
         assert_array_almost_equal(binary.G, np.array([[-2, 2],
                                                       [ 2,-2]]))
         rate3, rate4 = 6, 10
-        binary.update_rates(k_1=rate3, k_2=rate4)
+        binary.update_rates(dict(k_1=rate3, k_2=rate4))
         assert_equal(binary.rates[0][1], rate3)
         assert_equal(binary.rates[1][1], rate4)
         assert_array_almost_equal(binary.G, np.array([[-6, 10],
@@ -112,27 +113,32 @@ class TestBinarySystem:
         X = 'A'
         Y = 'B'
         mut_inf = binary.calculate_mutual_information(X, Y, base=2)
+        mut_inf2 = binary.calculate_instantaneous_mutual_information(X, Y, base=2)
 
         correct_values = np.array([0., 0.43858457, 0.6457636 , 0.77025155, 0.84901701,
                                    0.90004559, 0.93353366, 0.95567906, 0.9703932 , 0.98019935])
         assert_array_almost_equal(mut_inf[0::10], correct_values)
+        assert_array_almost_equal(mut_inf2[0::10], correct_values)
 
     def test_mutual_information_many_body(self, binary_many_body):
 
         X = 'A'
         Y = 'B'
         mut_inf = binary_many_body.calculate_mutual_information(X, Y, base=2)
+        mut_inf2 = binary_many_body.calculate_instantaneous_mutual_information(X, Y, base=2)
 
         correct_values = np.array([0., 0.92385593, 1.28269609, 1.48006893, 1.59832796,
                                    1.67229091, 1.71971752, 1.75060155, 1.77091263, 1.78435647])
+
         assert_array_almost_equal(mut_inf[0::10], correct_values)
+        assert_array_almost_equal(mut_inf2[0::10], correct_values)
 
 
 class Test2IsolatedSwitch:
 
     @pytest.fixture(scope='class')
     def isolated_switches(self):
-        system = c3s.simulators.MasterEquation(cfg='config_files/2_isolated_switches.yml', A=1, B=1)
+        system = CME(cfg='config_files/2_isolated_switches.yml', initial_populations={'A':1, 'B':1})
         start, stop, step = 0, 1, 0.01
         system.run(start, stop, step)
         yield system
